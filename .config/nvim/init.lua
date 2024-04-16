fb223d2428d369f7d66390b192d0230445014c8f
Mark Pitman <mark@pitman.io>
2024-04-15 23:37:15 -0700

    Initial Neovim configuration

---
+++ .config/nvim/init.lua
@@ -0,0 +1,18 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.opt.termguicolors = true
+vim.g.have_nerd_font = true
+vim.wo.number = true
+vim.opt.cursorline = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = false
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+
+require("plugins")
+require("nvim-tree").setup()
+require("whichkey")
+
